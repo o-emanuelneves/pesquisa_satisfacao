fd83397c7dc224ab79347dec651740c46754b159
sava7reg <gustavoherique1997@gmail.com>
2023-05-15 08:55:50 -0300

    tabela e muito chato

--- app/Views/auth_users/index.php
+++ app/Views/auth_users/index.php
@@ -1,32 +1,61 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Users</title>
+    <title>Lista de Usuários</title>
+
+    <link rel="stylesheet" href="/plugin/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="/assets/css/listusers.css">
+
+
 </head>
 
 <body>
+
+<div class="container-modal">
+
+
+
     <h1>Tela de listar todos os usuários</h1>
-    <a href="/AuthUsers/novo">Cadastrar novo</a>
+
+    <div class="new-users"><a href="/AuthUsers/novo">Cadastrar novo</a></div>
+
+
+<div class="style-table"> 
+
     <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Ações</th>
-            </tr>
-        </thead>
+
+    <div class="header-list">
+    
+                <thead>
+                     <tr>
+                              <th>ID</th>
+                              <th>Nome</th>
+                              <th>Ações</th>
+                      </tr>
+                </thead>
+    </div>
+
+
+
         <tbody>
             <?php foreach ($auth_users as $auth_user) : ?>
                 <tr>
-                    <td><?= $auth_user['id_user'] ?></td>
+                    <div class="users-id">
+                        <td><?= $auth_user['id_user'] ?></td>
+                    </div>
                     <td><?= $auth_user['nome'] ?></td>
                     <td>
-                        <a href="/AuthUsers/excluir/<?= $auth_user['id_user'] ?>">Excluir</a>
-                        <a href="/AuthUsers/ver/<?= $auth_user['id_user'] ?>">Ver</a>
+<div class="bottons-table">
+    
+                        <div class="botton-excluir">   <a href="/AuthUsers/excluir/<?= $auth_user['id_user'] ?>">Excluir</a></div>
+    
+    
+                        <div class="botton-ver"><a href="/AuthUsers/ver/<?= $auth_user['id_user'] ?>">Ver</a></div>
+</div>
 
                     </td>
                 </tr>
@@ -34,6 +63,11 @@
             <?php endforeach; ?>
         </tbody>
     </table>
+
+   </div>
+</div>
+
+
 </body>
 
 </html>
--- app/Views/inicio/index.php
+++ app/Views/inicio/index.php
@@ -1,18 +1,41 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tela de Início</title>
+
+    <link rel="stylesheet" href="/plugin/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="/assets/css/index.css">
+   
+
+
 </head>
 
 <body>
-    <h3>Tela de início.</h3>
-    <a href="/AuthUsers">Usuários</a> <br>
-    <a href="/AuthUsers/novo">Novo Usuário</a> <br>
-    <a href="/PesquisaPerguntas/novo">Criar Perguntas</a>
+
+<div class="container-modal">
+
+
+
+    <h1>Tela de início</h1>
+
+
+    <div class="containers-buttons-index">
+    
+        <div class="button-users"><a href="/AuthUsers">Usuários</a> <br></div>
+        <div class="button-new-users"><a href="/AuthUsers/novo">Novo Usuário</a> <br></div>
+        <div class="button-make-a-questions"><a href="/PesquisaPerguntas/novo">Criar Perguntas</a></div>
+
+    </div>
+
+    <div class="gif"><iframe src="https://giphy.com/embed/9gONVX546SZADKQxIy" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/CanalPremiere-premiere-brasileiro-canalpremiere-9gONVX546SZADKQxIy"></a></p></div>
+
+
+</div>
+
 </body>
 
 </html>
