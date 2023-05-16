36e4c1d977d6b79bb7e000e97641201157b82774
sava7reg <gustavoherique1997@gmail.com>
2023-05-16 08:20:16 -0300

    1605 gustavos

--- app/Views/pesquisa_perguntas/novo.php
+++ app/Views/pesquisa_perguntas/novo.php
@@ -1,119 +1,74 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Add Pergunta</title>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Pergunta</title>
+        <link rel="stylesheet" href="/plugin/fontawesome/css/all.min.css">
+        <link rel="stylesheet" href="/assets/css/questions.css">
+    </head>
+    <body>
 
-    
-   <link rel="stylesheet" href="/plugin/fontawesome/css/all.min.css">
-    <link rel="stylesheet" href="/assets/css/questions.css">
-
-
-    
-
-  
-</head>
-
-<body>
-<input type="number" class="usersid"> name="fk_user"> </div>
-
-
-
-
-
-<div class="container-modal">
-    
-     <form action="/PesquisaPerguntas/store" method="post">
- 
-        
-            <h1 class="left-align">Perguntas</h1>
-
-
-            <div class="btn-plus">
-                <div class="button-plus">
-                    <i class="fa-solid fa-plus"></i>
+        <div class="container-modal">
+            <form action="/PesquisaPerguntas/store" method="post">
+                <h1 class="left-align">Perguntas</h1>
+                <div class="btn-plus">
+                    <div class="button-plus">
+                        <i class="fa-solid fa-plus"></i>
+                    </div>
                 </div>
-            </div>
 
-            
-            <div class="questions">
-                
-                <label for="pergunta"></label>           
-                <input type="text" placeholder="Pergunta 1" name="pergunta[]" class="container-questions" id='pergunta'> 
-                <i class="fa-solid fa-trash-can"></i>           
-            
+                <div class="all-questions">
 
+                </div>
 
-            </div>
-
-
-            <div class="questions">
-
-                <form action="/PesquisaPerguntas/store" method="post">
-                <label for="pergunta2"></label>
-                <input type="text" placeholder="Pergunta 2" name="pergunta[]" class="container-questions" id='pesquisa2'>
-                <i class="fa-solid fa-trash-can"></i>              
-                
-                
-            </div>
-
-
-            <div class="questions">
-                <form action="/PesquisaPerguntas/store" method="post">
-                <label for="pergunta3"></label>
-                <input type="text" placeholder="Pergunta 3" name="pergunta[]" class="container-questions" id='pergunta3'>
-                <i class="fa-solid fa-trash-can"></i>
-              
-            </div>
-
-
-            <div class="questions">
-                <form action="/PesquisaPerguntas/store" method="post"> 
-                <label for="pergunta4"></label>     
-                <input type="text" placeholder="Pergunta 4" name="pergunta[]"class="container-questions" id='pergunta4'>
-                <i class="fa-solid fa-trash-can"></i>
-                          
-            </div>
-        
-
-            <div class="questions">
-                <form action="/PesquisaPerguntas/store" method="post">
-                <label for="pergunta5"></label>
-                <input type="text" placeholder="Pergunta 5" name="pergunta[]" class="container-questions" id='pergunta5'>
-                <i class="fa-solid fa-trash-can"></i>
-              
-            </div>
-
-            
-      
-    
-             
-             
-             
-
-
-
-
-
-        <div class="button">
-            <label for="fk_user">
-                
-            </label>
-            
-            
-                <input type="submit" value="Salvar" class="button-save">
-           
+                <div class="button">
+                    <label for="fk_user">
+                    </label>
+                    <input type="submit" value="Salvar" class="button-save">
+                </div>
+            </form>
         </div>
 
-
-    </form>
-
-
-</div>
-
-</body>
-
+        <div class="questionTemplate d-none">    
+            <label for=""></label>           
+            <input type="text" placeholder="" name="pergunta[]" class="container-questions"> 
+            <i class="fa-solid fa-trash-can delete-question"></i>           
+        </div>
+    </body>
 </html>
+
+<script>
+
+    const buttonPlus = document.querySelectorAll('.button-plus')[0];
+
+    buttonPlus.addEventListener('click', function () {
+        const questionTemplate = document.querySelectorAll('.questionTemplate')[0];
+        const questions = document.querySelectorAll('.questions');
+        const countQuestions = questions.length + 1 ?? 1;
+
+        const clone = questionTemplate.cloneNode(true);
+        const id = `perguntas-${countQuestions}`;
+        const place = `Pergunta ${countQuestions}`;
+        const allQuestions = document.querySelectorAll('.all-questions')[0];
+
+        clone.classList.remove('d-none');
+        clone.classList.remove('questionTemplate');
+        clone.setAttribute('id', id);
+        clone.setAttribute('class', 'questions');
+        clone.querySelector('label').setAttribute('for', id);
+        clone.querySelector('input').setAttribute('placeholder', place);
+        allQuestions.append(clone);
+
+        const buttonDelete = clone.querySelector('.delete-question');
+
+        buttonDelete.addEventListener('click', function () {
+            const divDelete = this.parentNode;
+            divDelete.remove();
+        });
+    })
+
+
+</script>
